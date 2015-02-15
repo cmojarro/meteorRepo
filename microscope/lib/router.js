8e1b370cb49308b16b52ee7c30b955c1d0135300
cmojarro <cmo@711-17.att.net>
2015-02-15 00:14:38 -0600

    Routing to a single post page

--- microscope/lib/router.js
+++ microscope/lib/router.js
@@ -4,3 +4,7 @@ loadingTemplate: 'loading',
 waitOn: function() { return Meteor.subscribe('posts'); }
 });
 Router.route('/', {name: 'postsList'});
+Router.route('/posts/:_id', {
+name: 'postPage',
+data: function() { return Posts.findOne(this.params._id); }
+});
